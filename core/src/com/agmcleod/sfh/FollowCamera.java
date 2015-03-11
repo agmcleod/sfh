29d113beeb88e9e5a6735fb21967bd7339e50131
Aaron McLeod <sircoolguy99@gmail.com>
2015-03-10 23:14:11 -0400

    fixed cam

--- core/src/com/agmcleod/sfh/FollowCamera.java
+++ core/src/com/agmcleod/sfh/FollowCamera.java
@@ -26,20 +26,20 @@ public class FollowCamera {
     public void followH(Vector2 target) {
         Camera cam = this.camera;
         if ((target.x - cam.position.x - camera.viewportWidth / 2) > (deadzone.x + deadzone.width)) {
-            cam.position.x = (float) Math.floor(Math.min((target.x) - (deadzone.x + deadzone.width), totalViewBounds.width - camera.viewportWidth));
+            cam.position.x = (float) Math.floor(Math.min(target.x - (deadzone.x + deadzone.width), totalViewBounds.width - camera.viewportWidth));
         }
         else if ((target.x - cam.position.x - camera.viewportWidth / 2) < (deadzone.x)) {
-            cam.position.x = (float) Math.floor(Math.max((target.x) - deadzone.x, camera.position.x));
+            cam.position.x = (float) Math.floor(Math.max(target.x - deadzone.x, totalViewBounds.x + camera.viewportWidth / 2));
         }
     }
 
     public void followV(Vector2 target) {
         Camera cam = this.camera;
         if ((target.y - cam.position.y - cam.viewportHeight / 2) > (deadzone.y + deadzone.height)) {
-            cam.position.y = (float) Math.floor(Math.min((target.y) - (deadzone.y + deadzone.height), totalViewBounds.height - camera.viewportHeight));
+            cam.position.y = (float) Math.floor(Math.min(target.y - (deadzone.y + deadzone.height), totalViewBounds.height - camera.viewportHeight));
         }
         else if ((target.y - cam.position.y - cam.viewportHeight / 2) < (deadzone.y)) {
-            cam.position.y = (float) Math.floor(Math.max((target.y) - this.deadzone.y, cam.position.y));
+            cam.position.y = (float) Math.floor(Math.max(target.y - this.deadzone.y, totalViewBounds.y + camera.viewportHeight / 2));
         }
     }
 
